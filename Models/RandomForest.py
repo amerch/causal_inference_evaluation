c6ba8b6b4c3cc913a8b5acdaecb67d866fde9be4
Suproteem Sarkar <suproteemsarkar@g.harvard.edu>
2019-04-30 18:38:15 -0400

    added classification to naive RF

--- Models/RandomForest.py
+++ Models/RandomForest.py
@@ -1,23 +1,39 @@
 from .Model import Model
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
 class RandomForest(Model):
-    def __init__(self):
-        self.reg = None
+	def __init__(self, *args, **kwargs):
+		self.reg = None
+		super(RandomForest, self).__init__(*args, **kwargs)
 
-    def fit(self, x, t, y, nfolds=5, seed=282):
-        # splits = super().get_splits(x, nfolds, seed)
-        self.reg = RandomForestRegressor(max_depth=3,
-                        n_estimators=100,
-                        random_state=282,)\
-                    .fit(self.get_predictors(x, t), y)
+	def fit(self, x, t, y, nfolds=5, seed=282):
+		# splits = super().get_splits(x, nfolds, seed)
 
-    def predict(self, x, t):
-        if self.reg is None:
-            raise Exception('RandomForest not Initialized')
+		#### CLASSIFICATION ####
+		if self.binary:
+			self.reg = RandomForestClassifier(max_depth=3,
+							n_estimators=100,
+							random_state=282,)\
+						.fit(self.get_predictors(x, t), y)
 
-        return self.reg.predict(self.get_predictors(x, t))
+		#### REGRESSION ####
+		else: 
+			self.reg = RandomForestRegressor(max_depth=3,
+							n_estimators=100,
+							random_state=282,)\
+						.fit(self.get_predictors(x, t), y)
 
-    def get_predictors(self, x, t):
-        return np.hstack([x, (t - 0.5).reshape(-1, 1) * x])
+	def predict(self, x, t):
+		if self.reg is None:
+			raise Exception('RandomForest not Initialized')
+
+		#### CLASSIFICATION ####
+		if self.binary:
+			return self.reg.predict_proba(self.get_predictors(x, t))[:, 1]
+		#### REGRESSION ####
+		else:    
+			return self.reg.predict(self.get_predictors(x, t))
+
+	def get_predictors(self, x, t):
+		return np.hstack([x, (t - 0.5).reshape(-1, 1) * x])
