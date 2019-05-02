b933dd6486e0a988bcffd3d2dd2712cd6d924522
amerch <amilmerchant@college.harvard.edu>
2019-05-01 20:48:32 -0400

    Updated sciprt file

--- Scripts/Convert+Twins.py
+++ Scripts/Convert+Twins.py
@@ -9,6 +9,7 @@
 
 # In[1]:
 
+
 import numpy as np
 import pandas as pd
 from scipy.special import expit
@@ -19,12 +20,14 @@ import json
 
 # In[2]:
 
+
 df = pd.read_csv('../Raw_Data/TWINS/twin_pairs_X_3years_samesex.csv')
 ys = pd.read_csv('../Raw_Data/TWINS/twin_pairs_Y_3years_samesex.csv')
 ys = ys.drop(['Unnamed: 0'], axis=1)
 
 
 # In[3]:
+
 
 desc = open('../Raw_Data/TWINS/covar_desc.txt', 'r').read()
 desc = eval(desc)
@@ -37,10 +40,12 @@ types['gestat10'] = 'ord'
 
 # In[4]:
 
+
 df.nunique()
 
 
 # In[5]:
+
 
 df['bord'] = (df['bord_0'] < df['bord_1']).astype(int)
 to_remove = ['Unnamed: 0', 'Unnamed: 0.1', 'infant_id_0', 'infant_id_1',
@@ -54,6 +59,7 @@ for var in to_remove + ['gestat10']:
 
 # In[6]:
 
+
 group_vars = {}
 for key, value in types.items():
     group_vars[value] = group_vars.get(value, []) + [key]
@@ -61,15 +67,18 @@ for key, value in types.items():
 
 # In[7]:
 
+
 group_vars['cat']
 
 
 # In[8]:
 
+
 missing = df.isna().mean(axis=0) > 0.2
 
 
 # In[9]:
+
 
 max_values = (df.max(axis=0) + 1)[missing]
 print (max_values.shape)
@@ -77,11 +86,13 @@ print (max_values.shape)
 
 # In[10]:
 
+
 mode_values = df.mode(axis=0).iloc[0][np.logical_not(missing)]
 print (mode_values.shape)
 
 
 # In[11]:
+
 
 new_category = missing.index[missing]
 mode_category = missing.index[np.logical_not(missing)]
@@ -95,11 +106,13 @@ print (mode_category)
 
 # In[12]:
 
+
 df[new_category] = df[new_category].fillna(max_values, axis=0)
 df[mode_category] = df[mode_category].fillna(mode_values, axis=0)
 
 
 # In[13]:
+
 
 df = pd.get_dummies(df, columns=group_vars['cat'])
 print (df.shape)
@@ -108,11 +121,13 @@ print ("This is not the same as CEVAE but the closest we could get to the author
 
 # In[14]:
 
+
 z = df['gestat10'].values.reshape(-1,1)
 x = df.drop(['gestat10'], axis=1).values
 
 
 # In[15]:
+
 
 n = 5
     
@@ -125,6 +140,7 @@ ys = ys.values
 
 
 # In[16]:
+
 
 noises = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 for noise in noises:
@@ -175,9 +191,4 @@ for noise in noises:
 
     np.savez(path + '/train.npz', x=x_train, t=t_train, yf=yf_train, ycf=ycf_train, mu1=mu1_train, mu0=mu0_train)
     np.savez(path + '/test.npz', x=x_test, t=t_test, yf=yf_test, ycf=ycf_test, mu1=mu1_test, mu0=mu0_test)
-
-
-# In[ ]:
-
-
 
